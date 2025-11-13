bd03c458e0a22bba93babef183144465ec171627
Bente Bruurs <bentebruurs@acknowledge.nl>
2025-11-13 12:57:42 +0100

    site bente aangepast

--- bente.py
+++ bente.py
@@ -1,2 +1,13 @@
+from connection_with_db import get_cursor
+
 def start():
-    return "True"
+    print("Het is gelukt!")
+
+    myconn, mycursor = get_cursor()
+    mycursor.execute("""SELECT * FROM club WHERE country = 'Netherlands'""")
+    rows = mycursor.fetchall()
+    print("hoi")
+    keys = [i[0] for i in mycursor.description]
+    data = [dict(zip(keys, row)) for row in rows]
+
+    return data
